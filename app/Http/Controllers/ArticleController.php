80f049efb492e62aa1209f47955ce8a98dc9eebb
yunqing <739244941@qq.com>
2019-01-04 18:04:37 +0800

    添加文章

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -55,4 +55,27 @@ class ArticleController extends Controller
             return $this->fail(300);
         }
     }
+
+    public function addArticle()
+    {
+        $author_id = request()->input('author_id');
+        $title = request()->input('title');
+        $content = request()->input('content');
+        $class_id = request()->input('class_id');
+        if (!$author_id || !$title || !$content || !$class_id) {
+            return $this->fail(201);
+        }
+        $time = date('Y-m-d H:i:s',time());
+        $arr['author_id'] = $author_id;
+        $arr['title'] = $title;
+        $arr['content'] = $content;
+        $arr['class_id'] = $class_id;
+        $arr['time'] = $time;
+        $res = Article::addArticle($arr);
+        if($res){
+            $this->success();
+        }else{
+            $this->fail(300);
+        }
+    }
 }
